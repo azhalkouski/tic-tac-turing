bd30f688b8f052d688bdc91f7f67c56a17d1c254
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-02-08 00:02:50 +0300

    Create children routes

--- src/getRoutes.js
+++ src/getRoutes.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Template from './components/Template';
+import Home from './components/Home';
+import Profile from './components/Profile';
 
 const getRoutes = () => (
   <Route
     path='/'
     component={Template}
   >
-
+    <IndexRoute
+      component={Home}
+    />
+    <Route
+      path='/profile'
+      component={Profile}
+    />
   </Route>
 )
 

5930206d823db56d24857b7fef707ad5582707a6
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-02-08 00:17:13 +0300

    Implement Material-UI

--- src/components/Template/TemplateContainer.jsx
+++ src/components/Template/TemplateContainer.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#70d3ec',
+    },
+    accent1Color: {
+      main: 'rgba(0,0,0,.87)',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 class TemplateContainer extends Component {
 
   render() {
     return (
-      <div>
-        <header>
-          <h1>Tic Tac Turing</h1>
-        </header>
-        <main>
-          {this.props.children}
-        </main>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <header>
+            <h1>Tic Tac Turing</h1>
+            <Button variant="contained" color="primary">Button</Button>
+          </header>
+          <main>
+            {this.props.children}
+          </main>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }

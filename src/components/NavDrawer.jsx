c1ce5e87aa1607627c9cad291a592a4491863eb9
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-02-09 16:08:36 +0300

    Add nav drawer functionality

--- src/components/NavDrawer.jsx
+++ src/components/NavDrawer.jsx
@@ -6,13 +6,29 @@ import FloatingActionButton from '@material-ui/core/Fab';
 import MenuIcon from '@material-ui/icons/Menu';
 
 class NavDrawer extends Component {
+  state = {
+    open: false,
+  }
+
+  toggle = () => {
+    this.setState((prevState) => ({ open: !prevState.open }));
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
-        <FloatingActionButton color="primary" aria-label="Add">
+        <FloatingActionButton color="primary" aria-label="Add" onClick={this.toggle}>
           <MenuIcon />
         </FloatingActionButton>
-        <Drawer open>
+        <Drawer open={this.state.open}>
+          <div
+            style={{
+              height: '200px',
+              width: '100%',
+              backgroundColor: 'salmon'
+            }}
+          >LoginContainer</div>
           <MenuItem>Play</MenuItem>
           <Divider />
           <MenuItem>Profile</MenuItem>

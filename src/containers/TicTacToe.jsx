c303eb7fa399dbfb6879a88aeb4aadf2e32d8e81
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-02-12 16:51:46 +0300

    Implement Square component and its functionality

--- src/containers/TicTacToe.jsx
+++ src/containers/TicTacToe.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Stage } from 'react-konva';
-import { Board } from '../styled/StyledTicTacToe';
+import { Board, Squares } from '../styled/StyledTicTacToe';
 
 class TicTacToe extends Component {
 
   state = {
     rows: 3,
+    unit: 0,
+    size: 0,
     gameState: new Array(9).fill(false),
     ownMark: 'X',
     otherMark: 'O',
@@ -13,6 +15,7 @@ class TicTacToe extends Component {
     yourTurn: true,
     winner: false,
     win: false,
+    coordinates: [],
   }
 
   componentDidMount() {
@@ -21,14 +24,23 @@ class TicTacToe extends Component {
     const size = height < width ? height * .8 : width * .8;
     const rows = this.state.rows;
     const unit = size / rows;
+    let coordinates = [];
+
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < rows; x++) {
+        coordinates.push([x*unit, y*unit]);
+      }
+    }
 
     this.setState({
       size,
       unit,
+      coordinates,
     });
   }
 
-  move = () => {
+  move = (marker, index) => {
+    console.log('Move made ', marker, index);
     // placeholder
   }
 
@@ -41,7 +53,18 @@ class TicTacToe extends Component {
   recordGame = () => {}
 
   render() {
-    const { unit, rows, size } = this.state;
+    const {
+      unit,
+      rows,
+      size,
+      coordinates,
+      gameState,
+      win,
+      gameOver,
+      yourTurn,
+      ownMark,
+    } = this.state;
+
     return (
       <div>
         <Stage
@@ -49,7 +72,16 @@ class TicTacToe extends Component {
           height={size}
         >
           <Board unit={unit} rows={rows} size={size} />
-          {/* <Squares /> */}
+          <Squares
+            unit={unit}
+            coordinates={coordinates}
+            gameState={gameState}
+            win={win}
+            gameOver={gameOver}
+            yourTurn={yourTurn}
+            ownMark={ownMark}
+            move={this.move}
+          />
         </Stage>
       </div>
     );
--- src/styled/StyledTicTacToe.jsx
+++ src/styled/StyledTicTacToe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layer, Line } from 'react-konva';
+import { Layer, Line, Text } from 'react-konva';
 
 export const Board = ({ unit, size, rows }) => {
   let grid = [];
@@ -33,6 +33,55 @@ export const Board = ({ unit, size, rows }) => {
   return (
     <Layer>
       {grid}
+    </Layer>
+  );
+};
+
+export const Squares = ({
+  unit,
+  coordinates,
+  gameState,
+  win,
+  gameOver,
+  yourTurn,
+  ownMark,
+  move,
+}) => {
+  const squares = coordinates.map((position, index) => {
+    let makeMove= move;
+    const mark = gameState[index];
+    let fill = 'black';
+    if (win && win.includes(index)) {
+      fill = 'lightgreen';
+    }
+    if (gameOver || !yourTurn || mark) {
+      makeMove = () => console.log('Nope!');
+    }
+
+    return (
+      <Text
+        key={index}
+        index={index}
+        x={position[0]}
+        y={position[1]}
+        fontSize={unit}
+        width={unit}
+        text={mark}
+        fill={fill}
+        fontFamily={'Helvetica'}
+        align={'center'}
+        onClick={(e) => {
+          const index = e.target.index;
+          console.log('onClick');
+          makeMove(index, ownMark);
+        }}
+      />
+    );
+  });
+
+  return (
+    <Layer>
+      {squares}
     </Layer>
   );
 };

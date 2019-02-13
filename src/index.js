7fe13fbe4d358ffc3a39735c35142bd70e075a87
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-02-13 14:49:52 +0300

    Development paused on 04:05 lesson

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, applyRouterMiddleware } from 'react-router';
 import Routes from './getRoutes';
 import Relay from 'react-relay';
-import userRelay from 'react-router-relay';
+import useRelay from 'react-router-relay';
 import { RelayNetworkLayer, urlMiddleware } from 'react-relay-network-layer';
 import { relayApi } from './config/endpoints';
-import auth from './utils/auth';
+// import auth from './utils/auth';
 
 import * as serviceWorker from './serviceWorker';
 
 const createHeaders = () => {
-  let idToken = auth.getToken()
-  if (idToken) {
-    return { Authorization: `Bearer ${idToken}` }
+  const token = 'very-strong-json-web-token';
+  if (token) {
+    return { Authorization: `Bearer ${token}` }
   } else {
     return {}
   }
-}
+};
+
+Relay.injectNetworkLayer(
+  new RelayNetworkLayer([
+    urlMiddleware({
+      url: (req) => relayApi,
+    }),
+    next => req => {
+      req.headers = {
+        ...req.headers,
+        ...createHeaders()
+      }
+      return next(req)
+    },
+  ],{disableBatchQuery: true})
+);
 
 ReactDOM.render(
   <Router
+    environment={Relay.Store}
+    render={applyRouterMiddleware(useRelay)}
     history={browserHistory}
     routes={Routes}
   />
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,74 +1,74 @@
-import Auth0Lock from 'auth0-lock';
-const authDomain = 'azhalkouski.eu.auth0.com';
-const clientId = 'QpzV4zDbcDH85mCRqlpxxQLg0irS3lKn';
+// import Auth0Lock from 'auth0-lock';
+// const authDomain = 'azhalkouski.eu.auth0.com';
+// const clientId = 'QpzV4zDbcDH85mCRqlpxxQLg0irS3lKn';
 
-class AuthService {
-  constructor() {
-    this.lock = new Auth0Lock(clientId, authDomain, {
-      auth: {
-        params: {
-          scope: 'openid email'
-        },
-      },
-    })
+// class AuthService {
+//   constructor() {
+//     this.lock = new Auth0Lock(clientId, authDomain, {
+//       auth: {
+//         params: {
+//           scope: 'openid email'
+//         },
+//       },
+//     })
 
-    this.showLock = this.showLock.bind(this);
-    this.lock.on('authenticated', this.authProcess.bind(this));
-  }
+//     this.showLock = this.showLock.bind(this);
+//     this.lock.on('authenticated', this.authProcess.bind(this));
+//   }
 
-  authProcess = (authResult) => {
-    console.log(authResult);
-  }
+//   authProcess = (authResult) => {
+//     console.log(authResult);
+//   }
 
-  showLock() {
-    this.lock.show();
-  }
+//   showLock() {
+//     this.lock.show();
+//   }
 
-  setToken = (authFields) => {
-    const {
-      idToken,
-      exp
-    } = authFields;
-    localStorage.setItem('idToken', idToken);
-    localStorage.setItem('exp', exp * 1000);
-  }
+//   setToken = (authFields) => {
+//     const {
+//       idToken,
+//       exp
+//     } = authFields;
+//     localStorage.setItem('idToken', idToken);
+//     localStorage.setItem('exp', exp * 1000);
+//   }
 
-  isCurrent = () => {
-    const expString = localStorage.getItem('exp');
-    if (!expString) {
-      localStorage.removeItem('idToken');
-      return false;
-    }
+//   isCurrent = () => {
+//     const expString = localStorage.getItem('exp');
+//     if (!expString) {
+//       localStorage.removeItem('idToken');
+//       return false;
+//     }
 
-    const now = new Date();
-    const exp = new Date(parseInt(expString, 10)); //10 is radix parameter
-    if ( exp < now ) {
-      this.logout();
-      return false;
-    } else {
-      return true;
-    }
-  }
+//     const now = new Date();
+//     const exp = new Date(parseInt(expString, 10)); //10 is radix parameter
+//     if ( exp < now ) {
+//       this.logout();
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
 
-  getToken() {
-    const idToken = localStorage.getItem('idToken');
-    if (this.isCurrent() && idToken) {
-      return idToken;
-    } else {
-      localStorage.removeItem('idToken');
-      localStorage.removeItem('exp');
-      return false;
-    }
-  }
+//   getToken() {
+//     const idToken = localStorage.getItem('idToken');
+//     if (this.isCurrent() && idToken) {
+//       return idToken;
+//     } else {
+//       localStorage.removeItem('idToken');
+//       localStorage.removeItem('exp');
+//       return false;
+//     }
+//   }
 
-  logout = () => {
-    localStorage.removeItem('idToken');
-    localStorage.removeItem('exp');
-    location.reload();
-  }
+//   logout = () => {
+//     localStorage.removeItem('idToken');
+//     localStorage.removeItem('exp');
+//     global.location.reload();
+//   }
 
-}
+// }
 
-const auth = new AuthService();
+// const auth = new AuthService();
 
-export default auth;
+// export default auth;
